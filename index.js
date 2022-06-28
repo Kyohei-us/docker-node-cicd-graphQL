cfc1fbfa04e9457efecbc8eca876e9bc6a245e9b
Kyohei-us <kyohei1212000@gmail.com>
2022-06-28 11:04:36 +0000

    Add: graphiql and basic schema

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema } from "graphql";
 
-console.log("import express, then end app.");
+const PORT = 3456;
+
+const schema = buildSchema(`
+    type Query {
+        message: String
+        messages: [String]
+    }
+`);
+
+const messages = ["1", "2", "3", "4"];
+
+const root = {
+  message: () => "Hello World!",
+  messages: messages,
+};
+
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Root is accessed.");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started at port: ${PORT}`);
+});
